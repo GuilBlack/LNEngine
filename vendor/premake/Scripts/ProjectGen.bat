242f2dd7abe9464f5f913ba2b079f510788eb35d
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2024-07-16 22:22:24 +0200

    add vulkan check when generating project

--- vendor/premake/Scripts/ProjectGen.bat
+++ vendor/premake/Scripts/ProjectGen.bat
@@ -1,3 +1,14 @@
+@echo off
 cd ..\..\..\
+
+if defined VULKAN_SDK (
+    echo Vulkan SDK is set to %VULKAN_SDK%
+) else (
+    echo Vulkan SDK is not set
+    echo Please set the environment variable VULKAN_SDK to the Vulkan SDK path
+    echo Example: set VULKAN_SDK=C:\VulkanSDK\version
+    PAUSE
+    exit
+)
 call vendor\premake\Binaries\premake5.exe vs2022
 PAUSE
