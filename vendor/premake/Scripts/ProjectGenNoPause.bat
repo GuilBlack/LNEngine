8de2316f02a33d290e7646a5671bba76ec004b01
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-11-26 15:45:15 +0100

    First Github Actions test

---
+++ vendor/premake/Scripts/ProjectGenNoPause.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if defined VULKAN_SDK (
+    echo Vulkan SDK is set to %VULKAN_SDK%
+) else (
+    echo Vulkan SDK is not set
+    echo Please set the environment variable VULKAN_SDK to the Vulkan SDK path
+    echo Example: set VULKAN_SDK=C:\VulkanSDK\version
+    PAUSE
+    exit
+)
+call vendor\premake\Binaries\premake5.exe vs2022
