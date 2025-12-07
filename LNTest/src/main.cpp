252b0063e680eaaf53fd333e1bb63e86cf092a3e
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2025-12-07 17:36:54 +0100

    Reapply "[Test] feat: create logging environment"
    This reverts commit 6c18fc4a5dd7559a22feb22339a67a08fa0fc82b.

--- LNTest/src/main.cpp
+++ LNTest/src/main.cpp
@@ -1,23 +1,8 @@
-#include "LNEInclude.h"
-
-TEST(SampleTest, BasicAssertions)
-{
-    // Expect two strings to be equal.
-    EXPECT_STREQ("hello", "hello");
-    // Expect equality.
-    EXPECT_EQ(1 + 1, 2);
-}
-
-TEST(SampleTest, FailingAssertions)
-{
-    // This assertion will fail.
-    EXPECT_NE(1 + 1, 3);
-    // This assertion will also fail.
-    EXPECT_STRNE("hello", "hello world");
-}
+#include "Environments/LogEnvironment.h"
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new LogEnvironment());
     return RUN_ALL_TESTS();
 }
